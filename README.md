7502d627be1279dcb2aea4247c45b86cffbd6d10
Shabd Saran <shabd.179301181@muj.manipal.edu>
2019-10-01 23:28:36 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# muj-dell-2019/hackathon
+
+Hackathon project for MUJ Dell Internship drive
+
+<br>
+
+### Backend Developers
+- [Shabd Saran](https://www.github.com/saran-shabd)
+- [Shikhar Vaish](https://www.github.com/svr8)
---
+++ src/test/java/com/mujdell2019/hackathon/HackathonApplicationTests.java
@@ -0,0 +1,16 @@
+package com.mujdell2019.hackathon;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HackathonApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
