03f33a93f5105e0d9119b67337ae766936d2cccd
Shabd Saran <shabd.179301181@muj.manipal.edu>
2019-10-02 01:47:24 +0530

    add user dao and encryption utilities

---
+++ src/main/java/com/mujdell2019/hackathon/dao/UserDAO.java
@@ -0,0 +1,63 @@
+package com.mujdell2019.hackathon.dao;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBQueryExpression;
+import com.mujdell2019.hackathon.models.UserDBModel;
+import com.mujdell2019.hackathon.utils.Encryption;
+
+@Component
+@Scope("singleton")
+public class UserDAO {
+
+	@Autowired
+	private ApplicationContext factory;
+	
+	// Amazon DynamoDB objects
+	private AmazonDynamoDB client = AmazonDynamoDBClientBuilder.standard().withRegion(Regions.AP_SOUTH_1).build();
+	private DynamoDBMapper mapper = new DynamoDBMapper(client);
+	
+	/*
+	 * check in DB whether user already exists with given user-name
+	 * */
+	public boolean exists(String username) {
+		
+		// search for user with given user-name in DB
+		UserDBModel user = new UserDBModel(username);
+		DynamoDBQueryExpression<UserDBModel> queryExpression = new DynamoDBQueryExpression<UserDBModel>().withHashKeyValues(user);
+
+		return 0 != mapper.count(UserDBModel.class, queryExpression);
+	}
+	
+	/*
+	 * create new user in DB
+	 * */
+	public void addUser(String username, String name, String password) {
+		
+		// hash password
+		String hashPassword = factory.getBean(Encryption.class).hash(password);
+		
+		// store user in DB
+		UserDBModel user = new UserDBModel(username, name, hashPassword);
+		mapper.save(user);
+	}
+	
+	/*
+	 * check whether password corresponding to given user-name matches the provided one or not
+	 * */
+	public boolean correctPassword(String username, String password) {
+		
+		// load user info from DB
+		UserDBModel user = mapper.load(UserDBModel.class, username);
+		
+		// compare passwords
+		return factory.getBean(Encryption.class).compareHash(user.getPassword(), password);
+	}
+}
---
+++ src/main/java/com/mujdell2019/hackathon/utils/Encryption.java
@@ -0,0 +1,76 @@
+package com.mujdell2019.hackathon.utils;
+
+import javax.crypto.Cipher;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+
+import org.apache.tomcat.util.codec.binary.Base64;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+import org.springframework.util.DigestUtils;
+
+@Component
+@Scope("singleton")
+public class Encryption {
+
+	@Value("${application.encryption.iv}")
+	private String encryptionIv;
+	
+	@Value("${application.encryption.key}")
+	private String encryptionKey;
+	
+	/*
+	 * member function to encrypt a plain text
+	 * */
+	public String encrypt(String plainText) {
+		try {
+			IvParameterSpec iv = new IvParameterSpec(encryptionIv.getBytes("UTF-8"));
+			SecretKeySpec secretKey = new SecretKeySpec(encryptionKey.getBytes("UTF-8"), "AES");
+		
+			Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5PADDING");
+			cipher.init(Cipher.ENCRYPT_MODE, secretKey, iv);
+			
+			byte[] encrypted = cipher.doFinal(plainText.getBytes());
+			
+			return Base64.encodeBase64String(encrypted);
+			
+		} catch (Exception e) {}
+	
+		return null;
+	}
+	
+	/*
+	 * member function to decrypt an encrypted text
+	 * */
+	public String decrypt(String encryptedText) {
+		try {
+			IvParameterSpec iv = new IvParameterSpec(encryptionIv.getBytes("UTF-8"));
+			SecretKeySpec secretKey = new SecretKeySpec(encryptionKey.getBytes("UTF-8"), "AES");
+		
+			Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5PADDING");
+			cipher.init(Cipher.DECRYPT_MODE, secretKey, iv);
+			
+			byte[] plainText = cipher.doFinal(Base64.decodeBase64(encryptedText));
+			
+			return new String(plainText);
+			
+		} catch (Exception e) {}
+		
+		return null;
+	}
+
+	/*
+	 * member function to hash a plain text, original text cannot be obtained once hashed
+	 * */
+	public String hash(String plainText) {
+		return DigestUtils.md5DigestAsHex(plainText.getBytes());
+	}
+	
+	/*
+	 * member function to compare a hashed text with a plain text
+	 * */
+	public boolean compareHash(String hash, String plainText) {
+		return hash.equals(hash(plainText));
+	}
+}
