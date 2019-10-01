7b781f5956bcb5c262e073c3d1b5a82b6ba650a0
Shabd Saran <shabd.179301181@muj.manipal.edu>
2019-10-02 00:52:15 +0530

    add dynamodb models of users, dellProducts and searchedProducts

---
+++ src/main/java/com/mujdell2019/hackathon/models/UserDBModel.java
@@ -0,0 +1,43 @@
+package com.mujdell2019.hackathon.models;
+
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBAttribute;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBHashKey;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBTable;
+
+@DynamoDBTable(tableName="users")
+public class UserDBModel {
+	
+	/* Data Members */
+
+	private String username;
+	private String name;
+	private String password;
+	
+	
+	/* Constructors */
+	
+	public UserDBModel() {}
+	
+	public UserDBModel(String username, String name, String password) {
+		this.username = username;
+		this.name = name;
+		this.password = password;
+	}
+	
+	public UserDBModel(String username) { this.username = username; }
+
+	
+	/* Getters and Setters */
+	
+	@DynamoDBHashKey(attributeName = "username")
+	public String getUsername() {return username; }
+	public void setUsername(String username) {this.username = username; }
+
+	@DynamoDBAttribute(attributeName = "name")
+	public String getName() { return name; }
+	public void setName(String name) { this.name = name; }
+
+	@DynamoDBAttribute(attributeName = "password")
+	public String getPassword() { return password; }
+	public void setPassword(String password) { this.password = password; }
+}
